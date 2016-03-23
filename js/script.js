a816f39e34cd520fefe36bd367044d5c5acff877
Manu Shergill <mandeeps@users.noreply.github.com>
2016-03-22 20:43:08 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function() {
   var curImg = 0, lastImg = 0, imgC = 0, list = [], lowRes = true;
-  var ALBUM_COUNT = 2; // ALBUM_COUNT * ALBUM_IMAGES pet photos should be enough for anybody!
+  var ALBUM_COUNT = 5; //2; // ALBUM_COUNT * ALBUM_IMAGES pet photos should be enough for anybody!
   var ALBUM_IMAGES = 56; // Specify images per album, in case Imgur changes this
   var offlineList = ['img/offline1.jpg', 'img/offline2.jpg', 'img/offline3.jpg',
    'img/offline4.jpg', 'img/offline5.jpg'];
@@ -213,7 +213,7 @@
   
   function online() {
     //console.log('now online');
-    if (!installed) {githubElem.css('display', 'block');}
+    //if (!installed) {githubElem.css('display', 'block');}
     if (!preloaded) {
       //notifyElem.html(click + ' photos for comments once loaded.'); //<br>Loading...
       notifyElem.html(click + " " + photoString);
